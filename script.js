cada3856fca5eff78def556950b1d0bae8a6f85c
Liam Donovan <ltdonova@ncsu.edu>
2021-05-20 01:11:59 -0400

    refactor and comments

--- script.js
+++ script.js
@@ -5,23 +5,26 @@ var button = document.querySelectorAll('.resButton');
 var containerLength = container.offsetHeight;
 var defaultLength = 16;
 
+// returns random hex value
 function getRandomColor() {
     return Math.floor(Math.random()*16777215).toString(16);
 
 }
+
+// gives sqaure div a new random background color
 function hoverColorChange(){
     let color = getRandomColor();
     this.style.backgroundColor = color;
-
-    //this.classList.add('squareHover');
 }
 
+// resets the square div back to default background color
 function removeHoverColorChange(){
     this.style.backgroundColor = "rgb(175, 204, 194)";
     // this.classList.remove('squareHover');
 
 }
 
+// iterates through all sqaures and setups Event Listeners color changing
 function addHoverEffect(){
     squares = document.querySelectorAll('.square');
 
@@ -31,13 +34,17 @@ function addHoverEffect(){
     });
 }
 
+// removes all square divs from container
 function removeSquares(){
     [...squares].forEach(element => { element.parentNode.removeChild(element)});
 }
 
+// calculates the length of a square div
 function calculateSquareLength(squareNum){
     return (containerLength / squareNum);
 }
+
+// creates the square div elements and places them into the container
 function createSquares(squareNum,squareLength){
     for(i=0; i<(squareNum*squareNum); i++){
         let square = document.createElement('div');
@@ -48,29 +55,31 @@ function createSquares(squareNum,squareLength){
     }
 }
 
-function createNewSketch(squareNum){
-    let squareLength = calculateSquareLength(squareNum);
-    createSquares(squareNum,squareLength);
-}
 
+// sets up new canvas for new square length
 function changeResolution(){
     var squareNum = prompt("Enter number of squares per side, limit is 100 ");
-    if(!squareNum)
-    return
+    if(!squareNum) 
+    return  // the user canceled the prompt so return
     removeSquares();
 
-    createNewSketch(squareNum);
+    let squareLength = calculateSquareLength(squareNum);
+    createSquares(squareNum,squareLength);
     addHoverEffect();
 }
 
 
-button[0].addEventListener('click',changeResolution);
 
+// create default canvas
 window.onload = () => {
     createSquares(defaultLength,calculateSquareLength(defaultLength));
     addHoverEffect();
     
 };
+
+// give button Event Lister for being clicked and to prompt resolution change
+button[0].addEventListener('click',changeResolution);
+
 
 
 
